2a2a52410894acfbc05cc99b22b6c25f82d766da
Matthew Plein <mjplein@gmail.com>
2018-06-11 17:13:25 -0500

    deploy

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter>
         <App />
-    </BrowserRouter>, document.getElementById('root'));
+    </HashRouter>, document.getElementById('root'));
 registerServiceWorker();
